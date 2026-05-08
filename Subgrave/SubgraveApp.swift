d60dbd4bea8557f577ba8e1425f97a306dea86dc
ahmetaksoy10 <a.aksoy1020@gmail.com>
2026-05-08 10:56:18 +0300

    Initial Commit

---
+++ Subgrave/SubgraveApp.swift
@@ -0,0 +1,17 @@
+//
+//  SubgraveApp.swift
+//  Subgrave
+//
+//  Created by MacBook Pro on 8.05.2026.
+//
+
+import SwiftUI
+
+@main
+struct SubgraveApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
